d5ee4e59276780d187ba88c4af8d79a951b0690c
someshnavale97 <someshnavale123@gmail.com>
2024-08-20 07:53:13 +0530

    somesh nawale twitter

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ app.get('/', (req, res) => {
 })
 
 app.get('/twitter', (req,res)=>{
-    res.send('someshnawale now on the twitter...!')
+    res.send('somesh nawale now on the twitter...!')
 })
 app.get('/login',(req,res)=>{
     res.send('<h1>Please login at ram hari gopal</h1>')
 })
 
 app.get('/youtube', (req,res) =>{
-    res.send('<h2>You can watch video.<h/2>')
+    res.send('<h2>You can watch video.</h2>')
 })
 app.get('/facebook',(req,res)=>{
     res.send('<h3>Facebook logge in success...!</h3>')
